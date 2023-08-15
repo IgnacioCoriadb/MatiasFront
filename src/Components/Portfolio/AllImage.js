d5a463c8cc20ea887890b9d432594c6d75793205
Ignacio <ignacioCoriadb@gmail.com>
2023-08-15 11:09:17 -0300

    delete image

--- src/Components/Portfolio/AllImage.js
+++ src/Components/Portfolio/AllImage.js
@@ -5,16 +5,20 @@ import ImageGallery from 'react-image-gallery';
 import 'react-image-gallery/styles/css/image-gallery.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import Swal from 'sweetalert2';
 
 const Image =({folder,modalOpen,setModalOpen})=>{
     const [image, setImage] = useState([]);
     const [imageOpened, setImageOpened] = useState(false);
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const token = localStorage.getItem('token');
 
     const allImages = async ()=>{
         const result = await axios.get(`http://localhost:3001/images/allImage/${folder}`);
         const imageUrls = result.data.map(url => ({
             original: url.url,
-            folderName: url.folderName
+            folderName: url.folderName,
+            id: url.id
         }));
         setImage(imageUrls);
     }
@@ -30,28 +34,82 @@ const Image =({folder,modalOpen,setModalOpen})=>{
         }
     };
 
+    const deleteImage = async(id) => {
+        const confirm = await Swal.fire({
+            title: '¿Estás seguro?',
+            text: 'Esta acción eliminará la imagen permanentemente.',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Eliminar',
+            cancelButtonText: 'Cancelar',
+            confirmButtonColor: '#d33',
+            cancelButtonColor: '#3085d6',
+          });
+
+        if (confirm.isConfirmed) {
+            try {
+                await axios.delete(`http://localhost:3001/images/deleteImage/${id}`, {
+                    headers: {
+                        'Authorization': `Bearer ${token}`,
+                    },
+                });
+                await allImages(); // Actualizar la lista de imágenes después de eliminar
+                setCurrentIndex(0);
+            } catch (error) {
+                if (error.response) {
+                    // Error de respuesta desde el servidor (estado HTTP no 2xx)
+                    console.error("Respuesta del servidor:", error.response.data);
+                    console.error("Código de estado:", error.response.status);
+                } else if (error.request) {
+                    // No se recibió respuesta del servidor
+                    console.error("No se recibió respuesta del servidor:", error.request);
+                } else {
+                    // Ocurrió un error durante la configuración de la solicitud
+                    console.error("Error durante la configuración de la solicitud:", error.message);
+                }
+                Swal.fire({
+                    position: 'center',
+                    icon: 'error',
+                    title: 'Error al enviar la solicitud',
+                    text: "No se pudo eliminar la imagen.",
+                    showConfirmButton: true,
+                });
+            }
+        }
+    }
+
+    const handleSlide = (index) => {
+        setCurrentIndex(index);
+    };
+
     useEffect(()=>{
         allImages();
         setImageOpened(true); 
         setModalOpen(false);
+        console.log(currentIndex)
     },[folder])
 
     return  (
         <>
           {modalOpen && (
             <div className={styles['image-modal']} onClick={handleBackgroundClick}>
-
-                <div className={styles['image-modal-margin']}>
+                <div className={`${styles['image-modal-margin']} ${token ? styles['logged-in'] : styles['logged-out']}`}>
                     <div className={styles['image-modal-content']}>
                     <button className={styles['close-button']} onClick={closeModal} style={{ zIndex: 999 }}>
                         <FontAwesomeIcon icon={faTimes} />
                     </button>
-                    {
-                        image[0]?.folderName && <h1 className={styles['title']}>{image[0].folderName}</h1>
-                    }                      
-                    {image.length > 0 && <ImageGallery items={image} />}
+                        {image.length > 0 && (
+                                <>
+                                    <h1 className={styles['title']}>{image[currentIndex].folderName}</h1>
+                                    {token && (
+                                        <button  className="btn btn-danger mt-2" onClick={() => deleteImage(image[currentIndex].id)}>Eliminar</button>
+                                    )}
+                                    <ImageGallery   items={image}
+                                        currentIndex={currentIndex}
+                                        onSlide={handleSlide} />
+                                </>
+                            )}
                     </div>
-
                 </div>
             </div>
         )}
